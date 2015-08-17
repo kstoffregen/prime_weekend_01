88bccdb3e7e5e8c9da1df0ebaf1268a8e0f8258a
kstoffregen <stoffrk@Kathryns-MacBook-Pro.local>
2015-08-17 12:13:02 -0500

    second commit. revise form functionality.

--- script.js
+++ script.js
@@ -1,11 +1,75 @@
-// var Employee = function(firstName, lastName, employeeNumber, title, reviewScore, salary){
-// 	this.firstName = firstName;
-// 	this.lastName = lastName;
-// 	this.employeeNumber = employeeNumber;
-// 	this.title = title;
-// 	this.reviewScore = reviewScore;
-// 	this.salary = salary;
-// }
+/* Make a form that can capture and store employees; First Name, Last Name, Employee Number, Title, Last Review Score, and Salary. Store that information in an object.*/
+/* Then, with that information captured, construct a list (UL) that populates with the most current employee information. Meaning that there should be a full list of employees represented on the screen.*/
+/* Not only should the employee information be present, but also add a 'remove' button grouped next to each employee’s information. Clicking that button should remove that specific employee.*/
+
+
+$(document).ready(function(){
+
+	var $form = $('#employeeInfo');
+	var $firstName = $('#firstName');
+	var $lastName = $('#lastName');
+	var $employeeNumber = $('#employeeNumber');
+	var $title = $('#title');
+	var $reviewScore = $('#reviewScore');
+	var $salary = $('#salary');
+	var $formOutput = $('#formOutput');
+
+	$form.on('submit', function(e){   //change ('#employeeInfo') to jQuery var
+		e.preventDefault();
+		var employee = new Employee (
+			$firstName.val(), $lastName.val(), $employeeNumber.val(), $title.val(), $reviewScore.val(), $salary.val()
+			);
+		appendEmployee(employee);
+	});
+
+	var Employee = function(firstName, lastName, employeeNumber, title, reviewScore, salary){
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.employeeNumber = employeeNumber;
+		this.title = title;
+		this.reviewScore = reviewScore;
+		this.salary = salary;
+	}
+
+	function appendEmployee(employee){
+		var $newEl = $('<li>');
+		$newEl.attr('class', 'reviewScore' + employee.reviewScore);   // if(employee.reviewScore == 5){$('employee.reviewScore').css('background-color', 'green');}
+		$newEl.text(employee.firstName + " " + employee.lastName + " " + employee.employeeNumber + " " + employee.title + " " + employee.reviewScore + " " + employee.salary);
+
+		var $button = $('<button>');    //removeButton = document.createElement('button'); removeButton.appendChild(buttonT); newEl.appendChild(removeButton);
+		$button.text('Remove Employee');   //buttonT = document.createTextNode("Remove Employee"); 
+		$button.attr('class', 'js-delete');
+
+		$newEl.append($button);
+		$formOutput.append($newEl);
+	}
+
+	$formOutput.on('click', '.js-delete', function(e){  //change ('#formOutput') to jQuery var
+			e.preventDefault;
+			$(this).parent().remove();
+			
+		});
+
+	
+
+
+});
+
+
+
+
+/* In the review score section, make sure to put some sort of color indicator based on their review. The employee reviews should only be able to be 1 - 5. 5 would indicate a good score, 1 would be poor.*/
+
+/* The system need not persist when the page refresh. Meaning that a page refresh will reload the entire screen. The submission should be as visually appealing as possible, and there will be some consideration of styling when you submission is reviewed.*/
+
+/* Hard Mode
+Include a button to generate a completely random employee. As one more step of hard mode, alphabetically sort the employees in the generated list.*/
+
+/* Pro Mode
+Include a section at the top of the page that totals the salaries of all the employees (Hint: the trick will be what happens when you remove an employee).*/
+
+
+// --- NOT USED ---
 
 // (function(){
 // 	var form = document.getElementById('employeeInfo');
@@ -18,13 +82,7 @@
 // 	});
 // }());
 
-	// var $inputFirstName = $('#firstName');
-	// var $inputLastName = $('#lastName');
-	// var $inputEmployeeNumber = $('#employeeNumber');
-	// var $inputTitle = $('#title');
-	// var $inputReviewScore = $('#reviewScore');
-	// var $inputSalary = $('#salary');
-	// var $div = $('#data');
+		// var $div = $('#data');
 
 		// employee[inputFirstName.attr('#firstName')] = $inputFirstName.val();
 		// employee[inputLastName.attr('lastName')] = $inputLastName.val();
@@ -40,80 +98,27 @@
 		// employee[this.reviewScore] = $(this).val();
 		// employee[this.salary] = $(this).val();
 
-		// $employee.firstName = elements.firstName.value;
-		// $employee.lastName = elements.lastName.value;
-		// $employee.employeeNumber = elements.employeeNumber.value;
-		// $employee.title = elements.title.value;
-		// $employee.reviewScore = elements.reviewScore.value;
-		// $employee.salary = elements.salary.value;		
-		// listOfEmployees.push($employee);
-		// var newEl = document.createElement('li');
+// employee.firstName = elements.firstName.value;
+// 		employee.lastName = elements.lastName.value;
+// 		employee.employeeNumber = elements.employeeNumber.value;
+// 		employee.title = elements.title.value;
+// 		employee.reviewScore = elements.reviewScore.value;
+// 		employee.salary = elements.salary.value;		
+
+// 		listOfEmployees.push(employee);
+// 		console.log(employee);
+
+// 		text(employee.firstName + " " + employee.lastName + " " + employee.employeeNumber + " " + employee.title + " " + employee.reviewScore + " " + employee.salary)
+
+		
 		// var newText = document.createTextNode("Full Name: " + $employee.firstName + " " + $employee.lastName + "\nEmployee Number: " + $employee.employeeNumber + "\nTitle: " + $employee.title + "\nReview Score: " + $employee.reviewScore + "\nSalary: " + $employee.salary);
 
 
-/* Make a form that can capture and store employees; First Name, Last Name, Employee Number, Title, Last Review Score, and Salary. Store that information in an object.*/
-/* Then, with that information captured, construct a list (UL) that populates with the most current employee information. Meaning that there should be a full list of employees represented on the screen.*/
+// position = document.getElementById('formOutput');
+// 		newEl = document.createElement('li');
 
-$(document).ready(function(){
+// 		newText = document.createTextNode(employee.firstName + " " + employee.lastName + " " + employee.employeeNumber + " " + employee.title + " " + employee.reviewScore + " " + employee.salary);
 
-	var form = document.getElementById('employeeInfo');
-	var listOfEmployees = [];
-	var employee = {};
-	var position, newEl, newText;
-	
+// 		newEl.appendChild(newText);
+// 		position.appendChild(newEl);
 
-	$('#employeeInfo').on('submit', function(event){
-		event.preventDefault();
-		var elements = this.elements;
-
-		employee.firstName = elements.firstName.value;
-		employee.lastName = elements.lastName.value;
-		employee.employeeNumber = elements.employeeNumber.value;
-		employee.title = elements.title.value;
-		employee.reviewScore = elements.reviewScore.value;
-		employee.salary = elements.salary.value;		
-
-		listOfEmployees.push(employee);
-		console.log(employee);
-		
-		position = document.getElementById('formOutput');
-		newEl = document.createElement('li');
-
-		newText = document.createTextNode(employee.firstName + " " + employee.lastName + " " + employee.employeeNumber + " " + employee.title + " " + employee.reviewScore + " " + employee.salary);
-
-		newEl.appendChild(newText);
-		position.appendChild(newEl);
-
-		var $removeButton = document.createElement('button');
-		var buttonT = document.createTextNode("Remove Employee");
-		$removeButton.appendChild(buttonT);
-		newEl.appendChild($removeButton);
-
-		$('#formOutput').on('click', $removeButton, function(){
-			position.removeChild(newEl);
-			
-		});
-	
-	});
-
-	
-
-		
-
-});
-
-	// removeButton = $('<button class="removeButton"></button>');
-
-/* Not only should the employee information be present, but also add a 'remove' button grouped next to each employee’s information. Clicking that button should remove that specific employee.*/
-
-
-
-/* In the review score section, make sure to put some sort of color indicator based on their review. The employee reviews should only be able to be 1 - 5. 5 would indicate a good score, 1 would be poor.*/
-
-/* The system need not persist when the page refresh. Meaning that a page refresh will reload the entire screen. The submission should be as visually appealing as possible, and there will be some consideration of styling when you submission is reviewed.*/
-
-/* Hard Mode
-Include a button to generate a completely random employee. As one more step of hard mode, alphabetically sort the employees in the generated list.*/
-
-/* Pro Mode
-Include a section at the top of the page that totals the salaries of all the employees (Hint: the trick will be what happens when you remove an employee).*/
